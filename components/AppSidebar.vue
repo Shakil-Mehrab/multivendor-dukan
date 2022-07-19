087a9b3e6df90bb8f15821a061a463adcb80f07a
Shakil-Mehrab <mehrabhoussainshakil12@gmail.com>
2022-07-19 20:16:08 +0600

    new

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -2,20 +2,21 @@
   <div
     class="hidden bg-navy-300 lg:flex lg:w-64 lg:flex-col lg:fixed lg:inset-y-0"
   >
-    <div
-      class="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto liner-gradient"
-    >
-      <div class="flex items-center flex-shrink-0 px-4">
+    <!--  py-2   border-b border-t  border-t-[rgba(255,255,255,.1)] border-b-black -->
+    <div class="flex flex-col flex-grow pb-4 overflow-y-auto liner-gradient">
+      <div
+        class="flex items-center flex-shrink-0 px-4 py-10 border-b border-gray-900 "
+      >
         <img
-          class="w-auto h-12 rounded-md"
+          class="w-auto h-10 rounded-md"
           src="@/assets/images/logo.svg"
           alt="Easywire logo"
         />
-        <span class="ml-2 text-xl font-bold text-gray-200">Online Shop</span>
+        <span class="ml-2 text-base font-bold text-gray-200">Online Shop</span>
       </div>
 
       <nav
-        class="flex flex-col flex-1 mt-5 overflow-y-auto divide-y"
+        class="flex flex-col flex-1 py-2 pt-12 overflow-y-auto border-t border-gray-700 "
         aria-label="Sidebar"
       >
         <div class="px-2 space-y-1">
@@ -24,11 +25,8 @@
             class="flex items-center px-2 py-2 text-sm font-medium leading-6 text-gray-100 rounded-md  group hover:text-white hover:bg-navy-200"
             :class="{ 'bg-navy-200': $route.name === 'index' }"
           >
-            <NewspaperIcon
-              class="w-6 h-6 mr-4 text-gray-200"
-              aria-hidden="true"
-            />
-            Articles
+            <HomeIcon class="w-6 h-6 mr-4 text-gray-200" aria-hidden="true" />
+            Home
           </nuxt-link>
 
           <nuxt-link
@@ -36,11 +34,11 @@
             class="flex items-center px-2 py-2 text-sm font-medium leading-6 text-gray-100 rounded-md  group hover:text-white hover:bg-navy-200"
             :class="{ 'bg-navy-200': $route.name === 'categories' }"
           >
-            <CollectionIcon
+            <ShoppingCartIcon
               class="w-6 h-6 mr-4 text-gray-200"
               aria-hidden="true"
             />
-            Categories
+            Orders
           </nuxt-link>
 
           <nuxt-link
@@ -48,11 +46,8 @@
             class="flex items-center px-2 py-2 text-sm font-medium leading-6 text-gray-100 rounded-md  group hover:text-white hover:bg-navy-200"
             :class="{ 'bg-navy-200': $route.name === 'topics' }"
           >
-            <BookOpenIcon
-              class="w-6 h-6 mr-4 text-gray-200"
-              aria-hidden="true"
-            />
-            Topics
+            <TruckIcon class="w-6 h-6 mr-4 text-gray-200" aria-hidden="true" />
+            Delivery
           </nuxt-link>
 
           <nuxt-link
@@ -60,11 +55,11 @@
             class="flex items-center px-2 py-2 text-sm font-medium leading-6 text-gray-100 rounded-md  group hover:text-white hover:bg-navy-200"
             :class="{ 'bg-navy-200': $route.name === 'regions' }"
           >
-            <LocationMarkerIcon
+            <ViewGridIcon
               class="w-6 h-6 mr-4 text-gray-200"
               aria-hidden="true"
             />
-            Regions
+            Products
           </nuxt-link>
 
           <nuxt-link
@@ -72,8 +67,8 @@
             class="flex items-center px-2 py-2 text-sm font-medium leading-6 text-gray-100 rounded-md  group hover:text-white hover:bg-navy-200"
             :class="{ 'bg-navy-200': $route.name === 'tags' }"
           >
-            <TagIcon class="w-6 h-6 mr-4 text-gray-200" aria-hidden="true" />
-            Tags
+            <CashIcon class="w-6 h-6 mr-4 text-gray-200" aria-hidden="true" />
+            Payments
           </nuxt-link>
 
           <nuxt-link
@@ -81,23 +76,11 @@
             class="flex items-center px-2 py-2 text-sm font-medium leading-6 text-gray-100 rounded-md  group hover:text-white hover:bg-navy-200"
             :class="{ 'bg-navy-200': $route.name === 'media' }"
           >
-            <DatabaseIcon
-              class="w-6 h-6 mr-4 text-gray-200"
-              aria-hidden="true"
-            />
-            Media
-          </nuxt-link>
-
-          <nuxt-link
-            :to="{}"
-            class="flex items-center px-2 py-2 text-sm font-medium leading-6 text-gray-100 rounded-md  group hover:text-white hover:bg-navy-200"
-            :class="{ 'bg-navy-200': $route.name === 'users' }"
-          >
             <UserGroupIcon
               class="w-6 h-6 mr-4 text-gray-200"
               aria-hidden="true"
             />
-            Users
+            Customers
           </nuxt-link>
         </div>
       </nav>
@@ -107,28 +90,23 @@
 
 <script>
 import {
-  CogIcon,
-  TagIcon,
-  CollectionIcon,
-  BookOpenIcon,
-  QuestionMarkCircleIcon,
-  ShieldCheckIcon,
-  DatabaseIcon,
+  HomeIcon,
+  ShoppingBag,
+  TruckIcon,
+  ViewGridIcon,
+  CashIcon,
   UserGroupIcon,
 } from "@vue-hero-icons/outline";
-import { LocationMarkerIcon, NewspaperIcon } from "@vue-hero-icons/solid";
+import { ShoppingCartIcon } from "@vue-hero-icons/solid";
 
 export default {
   components: {
-    CogIcon,
-    TagIcon,
-    LocationMarkerIcon,
-    CollectionIcon,
-    NewspaperIcon,
-    BookOpenIcon,
-    QuestionMarkCircleIcon,
-    ShieldCheckIcon,
-    DatabaseIcon,
+    HomeIcon,
+    ShoppingBag,
+    ShoppingCartIcon,
+    TruckIcon,
+    ViewGridIcon,
+    CashIcon,
     UserGroupIcon,
   },
 };
